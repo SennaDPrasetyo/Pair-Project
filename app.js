ef4814782fede56f6bfa2b937a57befddceba128
tiansyahmsalman <tiansyahisal@gmail.com>
2021-06-04 00:49:09 +0700

    patient

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require('./routes');
+const multer = require('multer');
+const path = require('path');
+const session = require('express-session');
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname)
+  }
+})
+
+const fileFilter = (req,file,cb) => {
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
+app.use(session({
+  secret: 'any',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('photo'))
+
+app.use(router)
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ migrations/20210603135929-remove-column-prescription.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up:  (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.removeColumn('Doctors', 'prescription')
+  },
+
+  down:  (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.addColumn('Doctors', 'prescription', Sequelize.STRING)
+  }
+};
